f2744f2d3886ce177b173e87507761420b8b44e7
uddesh45 <120295086+uddesh45@users.noreply.github.com>
2024-06-21 15:29:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import ExpenceTracker from "./component/ExpenceTracker"
+function App() {
+  return (
+    <div className="App">
+      <ExpenceTracker/>
+    </div>
+  );
+}
+
+export default App;
